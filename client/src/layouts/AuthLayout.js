ea89809abaafe411fcdd90626f9a92103448a9d4
Tapish <tapish@Tapishs-MacBook-Air.local>
2025-05-03 19:34:37 +0530

    ui changes

--- client/src/layouts/AuthLayout.js
+++ client/src/layouts/AuthLayout.js
@@ -4,7 +4,7 @@ import { Outlet, Link } from "react-router-dom";
 function AuthLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <div className="flex-grow flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="flex-grow flex flex-col justify-center py-3 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <Link to="/" className="flex justify-center">
             <h2 className="text-center text-3xl font-extrabold text-primary-600">
@@ -13,8 +13,8 @@ function AuthLayout() {
           </Link>
         </div>
 
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <div className="mt-3 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white shadow sm:rounded-lg">
             <Outlet />
           </div>
         </div>
